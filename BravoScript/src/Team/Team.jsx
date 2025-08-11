9dc26bae6dc27c65cdcaf18d73b9a772fe723877
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-08-11 18:09:52 +0530

    Update team section

--- BravoScript/src/Team/Team.jsx
+++ BravoScript/src/Team/Team.jsx
@@ -17,71 +17,73 @@ const Team = () => {
       <hr style={{ border: "1px solid black", margin: "20px 0" }} />
 
       <div className="Team-contain">
-              <div className="Team-text">
-                <img src={text} alt="Team Heading" />
-                <h1>The team behind BravoScript</h1>
-                <p>
-                  We work to create the most attractive & meaningful for small businesses. Our team always ready to help you.
-                </p>
+        <div className="Team-text">
+          <img className='head' src={text} alt="Team Heading" />
+          <h1>The team behind BravoScript</h1>
+          <p>
+            We work to create the most attractive & meaningful for small businesses. Our team always ready to help you.
+          </p>
+        </div>
+
+        <div className="Team-cards">
+          <div className="Team-Details">
+            <div class="card">
+              <div class="background">
+                <img className='member' src={per1} alt='' />
               </div>
+              <a href="#"><div class="box box1"><span class="icon"><img className='svg' src={instagram}/></span></div></a>
+              <a href="##"><div class="box box2"> <span class="icon"><img className='svg' src={facebook}/></span></div></a>
 
-              <div className="Team-cards">
-                <div className="Team-Details">
-                  <img src={per1} alt=''/>
-                  <h6>Sandith Kariyawasam</h6>
-                  <p>Founder & Chief Executive Officer</p>
-
-                  <div className="Team-social">
-                    <img src={facebook} alt=''/>
-                    <img src={instagram} alt=''/>
-                    <img src={x} alt=''/>
-                  </div>
-                </div>
-
-                <div className="Team-Details">
-                  <img src={per2} alt=''/>
-                  <h6>Mark Zuckerburge</h6>
-                  <p>Senior Vice President & Chief Technology Officer</p>
-
-                  <div className="Team-social">
-                  <img src={facebook} alt=''/>
-                    <img src={instagram} alt=''/>
-                    <img src={x} alt=''/>
-                  </div>
-                </div>
-
-                <div className="Team-Details">
-                  <img src={per3} alt=''/>
-                  <h6>Rose Paul</h6>
-                  <p>Senior Vice President & Chief Revenue Officer</p>
-
-                  <div className="Team-social">
-                    <img src={facebook} alt=''/>
-                    <img src={instagram} alt=''/>
-                    <img src={x} alt=''/>
-                  </div>
-                </div>
-
-                <div className="Team-Details">
-                  <img src={per4} alt=''/>
-                  <h6>Burnado Silva</h6>
-                  <p>Senior Vice President & Global Head of Sales</p>
-
-                  <div className="Team-social">
-                    <img src={facebook} alt=''/>
-                    <img src={instagram} alt=''/>
-                    <img src={x} alt=''/>
-                  </div>
-                </div>
-              </div>
-
-              <div className="Pro-btn">
-                <button class="hero-btn">
-                    <span>
-                        View All Members
-                    </span>
-                </button>
             </div>
+            <h6>Sandith Kariyawasam</h6>
+            <p>Founder & Chief Executive Officer</p>
+          </div>
+
+          <div className="Team-Details">
+            <div class="card">
+              <div class="background">
+                <img className='member' src={per2} alt='' />
+              </div>
+              <a href="#"><div class="box box1"><span class="icon"><img className='svg' src={instagram}/></span></div></a>
+              <a href="##"><div class="box box2"> <span class="icon"><img className='svg' src={facebook}/></span></div></a>
+
+            </div>
+            <h6>Mark Zuckerburge</h6>
+            <p>Senior Vice President & Chief Technology Officer</p>
+          </div>
+
+          <div className="Team-Details">
+            <div class="card">
+              <div class="background">
+                <img className='member' src={per3} alt='' />
+              </div>
+              <a href="#"><div class="box box1"><span class="icon"><img className='svg' src={instagram}/></span></div></a>
+              <a href="##"><div class="box box2"> <span class="icon"><img className='svg' src={facebook}/></span></div></a>
+            </div>
+            <h6>Rose Paul</h6>
+            <p>Senior Vice President & Chief Revenue Officer</p>
+          </div>
+
+          <div className="Team-Details">
+            <div class="card">
+              <div class="background">
+                <img className='member' src={per4} alt='' />
+              </div>
+              <a href="#"><div class="box box1"><span class="icon"><img className='svg' src={instagram}/></span></div></a>
+              <a href="##"><div class="box box2"> <span class="icon"><img className='svg' src={facebook}/></span></div></a>
+            </div>
+            <h6>Burnado Silva</h6>
+            <p>Senior Vice President & Global Head of Sales</p>
+          </div>
+        </div>
+
+        <div className="Pro-btn">
+          <button className="hero-btn">
+            <span>
+              View All Members
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   )
